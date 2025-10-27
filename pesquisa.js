06cfbe8a26b9a0f4462b047d626a6d8a62e321d8
unknown <vitohugocoleta2005@gmail.com>
2025-10-27 19:50:31 -0300

    Criado sobre.html que será usado para mostrar informações de um livro específico, pesquisa.css para pequenos detalhes de estilização, adicionado imagem-genérica.jpg para livros com capa indisponível. Moficado pesquisa.html com pequeno campo drop-down para escolher tipo de pesquisa e pesquisa.js com funções para pesquisa com título, autor ou geral, verificação simples se o campo foi preenchido, lógica para a escolha da capa mostrada na página.

--- pesquisa.js
+++ pesquisa.js
@@ -1,44 +1,83 @@
 const botao_pesquisa = document.getElementById("botao-pesquisa");
+const opcao_pesquisa = document.getElementById("opcao-pesquisa");
 const section_resultados = document.getElementById("resultados");
+const campo_pesquisa = document.getElementById("pesquisa-livro");
+const url = "https://openlibrary.org/search.json";
 
 botao_pesquisa.addEventListener("click", function(){
     section_resultados.innerHTML = "";
-    pesquisar_livro();
+    pesquisar_livros();
 });
 
-async function pesquisar_livro(){
-
-    let url = "https://openlibrary.org/search.json";
-    const campo_pesquisa = document.getElementById("pesquisa-livro");
-
-    await fetch(`${url}?q=${campo_pesquisa.value}`)
-        .then(response => response.json())
-        .then(response => {
-            let array_livros = response.docs;
-
-            console.log(array_livros);
-
-            array_livros.forEach(livro => {
-                let i=0;
-                
-                i++;
-
-                if (i==20)
-                    return;
-
-                section_resultados.innerHTML += `
-                <div> 
+function mostrar_livros(array_livros){
+     array_livros.forEach(livro => {
+        let url_imagem = '';    
+        
+        console.log(livro.cover_i);
+        if (livro.cover_i === undefined)
+                url_imagem = "imagem-gerenica.jpg"
+            else
+                url_imagem = `https://covers.openlibrary.org/b/olid/${livro.cover_edition_key}-M.jpg`
+            
+            
+            section_resultados.innerHTML += `
+            <div> 
                 <p>${livro.title}</p>
                 <p>${livro.author_name}</p>
                 <p>${livro.first_publish_year}</p>
                 <p>${livro.cover_i}</p>
-                <img src="https://covers.openlibrary.org/b/olid/${livro.cover_edition_key}-M.jpg"/>
+                <div class="capa-livro">
+                    <a href="sobre.html"><img src="${url_imagem}"/></a>
                 </div>
-                <br>
-                `
+            </div>
+            <br>
+            `
 
             });
+}
+
+async function pesquisar_livro_titulo(){
+
+    await fetch(`${url}?title=${campo_pesquisa.value}`)
+        .then(response => response.json())
+        .then(response => {
+            mostrar_livros(response.docs);
+            console.log(response.docs);
+        })
+}
+
+async function pesquisar_livro_autor(){
+
+    await fetch(`${url}?author=${campo_pesquisa.value}`)
+        .then(response => response.json())
+        .then(response => {
+            mostrar_livros(response.docs);
         });
+
+}
+
+async function pesquisar_livro_geral(){
+    await fetch(`${url}?q=${campo_pesquisa.value}`)
+}
+
+function campo_vazio(){
+    return campo_pesquisa.value.trim().length === 0;
+}
+
+function pesquisar_livros(){
+    console.log(campo_vazio());
+    if (campo_vazio() === false){
+        if (opcao_pesquisa.value === "titulo")
+            pesquisar_livro_titulo();                    
+        else if (opcao_pesquisa.value === "autor")
+            pesquisar_livro_autor();
+        else if (opcao_pesquisa.value === "geral"){
+            pesquisar_livro_geral();
+        }
+    }
+    else{
+        alert("Nada foi inserido no campo de pesquisa.");
+    }
 
 
 }
