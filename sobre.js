fd953c46b1b648aa12665076430336bbc4b8bf51
unknown <vitohugocoleta2005@gmail.com>
2025-11-06 18:18:51 -0300

    Somente estilização

--- sobre.js
+++ sobre.js
@@ -1,7 +1,7 @@
 
 const url = "https://openlibrary.org/books/";
 const urlCover = "https://covers.openlibrary.org/b/id/";
-const container = document.querySelector("div.container");
+const container = document.querySelector("div.container-sobre");
 
 // https://covers.openlibrary.org/b/id/12547191-L.jpg
 // response.authors[0].author.key
@@ -11,9 +11,20 @@ async function teste() {
     livro = JSON.parse(livro);
     console.log(livro)
     container.insertAdjacentHTML("afterbegin", `
-        <h1>${livro.title}</h1>
-        <img src="${urlCover}${livro.cover_i}-L.jpg">
-    
+        <div class="container-infos">
+            
+            
+            <div class="container-img-livro">
+                <img src="${urlCover}${livro.cover_i}-L.jpg">
+                <button class="add-button">Adicionar em Lidos</button>
+                <button class="add-button">Adicionar em Para Ler</button>
+            </div>
+            
+            <div class="info-texto">
+                <h1>${livro.title}</h1>
+                <h2>De:${livro.author_name}</h2>
+            </div>
+        <div>
     `)
 }
 
